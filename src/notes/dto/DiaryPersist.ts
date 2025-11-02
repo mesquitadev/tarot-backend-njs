fb1bf82ddd94ff79ea68280f8e84ceb54c6e3474
Paulo Victor Costa Mesquita Pinto <paulo.mesquita@grupomateus.com>
2025-11-02 19:48:40 -0300

    feat: atualizando o readme

--- src/notes/dto/DiaryPersist.ts
+++ src/notes/dto/DiaryPersist.ts
@@ -1,11 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class DiaryPersist {
   @IsNotEmpty({ message: 'O Título não pode ser vazio!' })
   title: string;
   @IsNotEmpty({ message: 'O Conteúdo não pode ser vazio!' })
   content: string;
-  @IsNotEmpty({message: 'A categoria é obrigatória!'})
+
+  @IsNotEmpty({ message: 'A categoria é obrigatória!' })
+  @IsString({ message: 'A categoria deve ser uma string!' })
   category: string;
   userId: number;
 }
