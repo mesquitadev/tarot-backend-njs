69a19a88459a2c03c372a2a47ec93b73ac98897d
Paulo Victor Costa Mesquita Pinto <paulo.mesquita@grupomateus.com>
2025-11-03 09:07:40 -0300

    feat: atualizando o readme

---
+++ prisma/migrations/20251103120056_remove_mood/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `mood` on the `Diary` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Diary" DROP COLUMN "mood";
